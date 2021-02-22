9949088155537ff37d6c02c3b326d58c6c9db916
Vision0123 <jlukhi0@gmail.com>
2021-02-22 18:56:33 +0530

    first Commit

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./route');
+const bodyParser= require('body-parser');
 
 //Connect to mongodb
 mongoose.connect("mongodb+srv://vision:12345@cluster0.bjch1.mongodb.net/student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         const app = express();
+        app.use(bodyParser.urlencoded({extended:true}));
+        app.use(express.json());
         app.use("/api",route);
 
         app.listen(process.env.PORT || 3000,()=>{
-            console.log("<--------Server Started-- ----->");
+            console.log("<--------Server Started------->");
         });
     }
 );
--- route.js
+++ route.js
@@ -3,9 +3,37 @@ const Book = require('./Model/book');
 const router = express.Router();
 
 
-router.get("/books",async(req,res)=>{
+router.get("/books",async (req,res)=>{
     const book = await Book.find();
     res.send(book);
 });
+
+router.post("/books",async (req,res)=>{
+
+    const book = new Book({
+        name:req.body.name,
+        qty:req.body.qty
+        
+    });
+    await book.save();
+    res.send(book);
+});
+
+// router.put("/books",async (req,res)=>{
+    
+// });
+
+router.delete("/books/:id",async(req,res)=>{
+
+    try {
+        await Book.deleteOne({_id:req.params.id}); 
+        res.send("Delete successfully");
+        
+    } catch (error) {
+        res.status(404).send({error:"Book not find"});
+    }
+
+});
+
 
 module.exports = router; 
